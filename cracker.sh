a2e09494f6e7dbf43d5386b66cedaa2d5f001347
JRodriguez556 <37945660+JRodriguez556@users.noreply.github.com>
2018-10-02 16:37:46 -0400

    Update cracker.sh

--- cracker.sh
+++ cracker.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
-filetype() {
-  echo "Is this a hashcat file or asleap C/R?"
-  echo "1: Hashcat"
-  echo "2: Asleap"
-##
-# echo "3: Aircrack (WPA-PSK)"
-##
-  read -r file_type
-  if [ "$file_type" = "1" ]; then
-    hashcatfile
-  elif [ "$file_type" = "2" ]; then
-    asleapfile
-##
-#  elif [ "$file_type" = "3"]; then
-#    aircracker
-##
-  else
-    echo "Please try again"
-    filetype
-  fi
-}
 
 hashcatfile() {
   echo "Please input the path to the file you want cracked. (/full/path)"
@@ -57,27 +36,4 @@ hashcatfile() {
   echo "Ended at $end"
 }
 
-asleapfile() {
-  echo "please input the Challenge"
-  read -r challenge
-  echo "Please input the Response"
-  read -r response
-  echo "Cracking with best options"
-  echo "SCRIPT WILL NOT STOP ON SUCCESSFUL CRACK, PLEASE MONITOR STDOUT"
-  sleep 3
-  /tools/hashcat/hashcat64.bin /wordlists/* --stdout| /opt/asleap/asleap -C "$challenge" -R "$response" -W -
-  /tools/hashcat/hashcat64.bin /wordlists/* -r /tools/hashcat/rules/best64.rule -w 3  --stdout| /opt/asleap/asleap -C "$challenge" -R "$response" -W -
-  /tools/hashcat/hashcat64.bin /wordlists/* -r /tools/hashcat/rules/d3ad0ne.rule -w 3 --stdout| /opt/asleap/asleap -C "$challenge" -R "$response" -W -
-  /tools/hashcat/hashcat64.bin /wordlists/* -r /tools/hashcat/rules/rockyou-30000.rule -w 3  --stdout| /opt/asleap/asleap -C "$challenge" -R "$response" -W -
-  /tools/hashcat/hashcat64.bin /wordlists/* -r /tools/hashcat/rules/combinator.rule -w 3  --stdout| /opt/asleap/asleap -C "$challenge" -R "$response" -W -
-  /tools/hashcat/hashcat64.bin /wordlists/* -r /tools/hashcat/rules/leetspeak.rule -w 3  --stdout| /opt/asleap/asleap -C "$challenge" -R "$response" -W -
-  /tools/hashcat/hashcat64.bin /wordlists/* -r /tools/hashcat/rules/unix-ninja-leetspeak.rule -w 3  --stdout| /opt/asleap/asleap -C "$challenge" -R "$response" -W -
-  /tools/hashcat/hashcat64.bin /tools/hashcat/masks/* -a 3 -w 3  --stdout| /opt/asleap/asleap -C "$challenge" -R "$response" -W -
-  echo "done cracking"
-}
-##
-#aircracker() {
-#
-#}
-##
-filetype
+hashcatfile
