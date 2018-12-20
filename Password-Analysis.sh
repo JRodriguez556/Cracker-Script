ce6d7fb2c67525e3f0d711a18fa630dca4b4ed26
JRodriguez556 <37945660+JRodriguez556@users.noreply.github.com>
2018-12-20 16:03:22 -0500

    Update Password-Analysis.sh

--- Password-Analysis.sh
+++ Password-Analysis.sh
@@ -52,8 +52,9 @@ gethashtype(){
 cracklm() {
  start_time=$(date +"%m-%d-%Y::%H:%M")
  echo "$start_time" > lm.start.time.del
- /tools/hashcat/hashcat64.bin -m 3000 "$hashcatfilelocation".lm.hashes.del /wordlists/* -w 3 --session "$hashcatfilelocation".lm.hashes.restore.1
- /tools/hashcat/hashcat64.bin -m 3000 "$hashcatfilelocation".lm.hashes.del /wordlists/* -r /tools/hashcat/rules/all.pwanalysis.rule -w 3 --session "$hashcatfilelocation".lm.hashes.restore.2
+ /tools/hashcat/hashcat64.bin -m 3000 "$hashcatfilelocation".lm.hashes.del -w 3 -a 3 -1 ?u?d?s --increment  ?1?1?1?1?1?1?1
+ #/tools/hashcat/hashcat64.bin -m 3000 "$hashcatfilelocation".lm.hashes.del /wordlists/* -w 3 --session "$hashcatfilelocation".lm.hashes.restore.1
+ #/tools/hashcat/hashcat64.bin -m 3000 "$hashcatfilelocation".lm.hashes.del /wordlists/* -r /tools/hashcat/rules/all.pwanalysis.rule -w 3 --session "$hashcatfilelocation".lm.hashes.restore.2
  #/tools/hashcat/hashcat64.bin -m 3000 "$hashcatfilelocation".lm.hashes.del /tools/hashcat/masks/* -w 3 -a 3 --session "$hashcatfilelocation".lm.hashes.restore.3
  end_time=$(date +"%m-%d-%Y::%H:%M")
  echo "$end_time" > lm.end.time.del
