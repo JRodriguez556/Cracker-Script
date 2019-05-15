c7904193f57988ab78bb8a0cc951358c4cb1bfa9
Jesse Rodriguez <me@rodriguezj.me>
2019-05-15 15:52:07 -0400

    Update

--- Password-Analysis.sh
+++ Password-Analysis.sh
@@ -82,7 +82,6 @@ crackntlm() {
  echo "$start_time" > ntlm.start.time."$main_start_time"
  /tools/hashcat/hashcat64.bin -m 1000 "$hashcatfilelocation".ntlm.hashes."$main_start_time" /wordlists/all.wordlists.clean -w 3 --session "$hashcatfilelocation".ntlm.hashes.restore.1
  /tools/hashcat/hashcat64.bin -m 1000 "$hashcatfilelocation".ntlm.hashes."$main_start_time" /wordlists/all.wordlists.clean -r /tools/hashcat/rules/all.pwanalysis.rule -w 3 --session "$hashcatfilelocation".ntlm.hashes.restore.2
- /tools/hashcat/hashcat64.bin -m 1000 "$hashcatfilelocation".ntlm.hashes."$main_start_time" -w 3 -a 3  --increment  ?a?a?a?a?a?a?a --session "$hashcatfilelocation".ntlm.hashes.restore.4
  /tools/hashcat/hashcat64.bin -m 1000 "$hashcatfilelocation".ntlm.hashes."$main_start_time" /tools/hashcat/masks/new.all.masks -w 3 -a 3 --session "$hashcatfilelocation".ntlm.hashes.restore.3
  end_time=$(date +"%m-%d-%Y::%H:%M")
  echo "$end_time" > ntlm.end.time."$main_start_time"
